381ff15a73cb40c356d0b7d2f37f4746174e51ac
Anthony Guerrero <adguerrero1935@gmail.com>
2021-05-13 15:32:43 -0400

    Added my component folder

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+
+
+function jumbotron() {
+    return (
+        <div>
+            <header>
+                <h1>
+                    Hello
+                </h1>
+            </header>
+        </div>
+    )
+};
+
+export default jumbotron;
