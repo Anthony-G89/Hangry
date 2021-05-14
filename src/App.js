e34db8605aa3ad39d0646ed72dfbc1b080b08055
Anthony Guerrero <adguerrero1935@gmail.com>
2021-05-14 12:46:07 -0400

    Styled my jumbotron and added my search bar, dropdown box for type, dropdown box for distances, and serach btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
+import SearchContainer from "./components/SearchContainer";
 
 function App() {
   return (
     <div >
       < Jumbotron />
+      < SearchContainer />
 
     </div>
   );
---
+++ src/components/SearchContainer/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import './style.css'
+
+
+function searchBar() {
+    return (
+        <div className={"col-lg-3 searchContainer"}>
+            <input className={"userInput"} type={"text"} placeholder={"Enter your city"} />
+
+            <select className={"formType"} aria-label={"Default select example"}>
+                <option value={"1"}>Resturants</option>
+                <option value={"2"}>Bars</option>
+                <option value={"3"}>Events</option>
+            </select>
+
+            <select className={"distanceType"} aria-label={"Distances"}>
+                <option value={"oneM"}>5 Miles</option>
+                <option value={"twoM"}>10 Miles</option>
+                <option value={"threeM"}>15 Miles</option>
+            </select>
+
+            <button className={"searchBtn"} type={"button"}>Search</button>
+
+        </div>
+    )
+};
+
+export default searchBar;
