26e5887b7af53bedcfd9100f425f58ace4977d60
Anthony Guerrero <adguerrero1935@gmail.com>
2021-05-16 15:59:31 -0400

    Working on the styling were the results will show up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
 import SearchContainer from "./components/SearchContainer";
+import SearchResults from "./components/SearchResults";
 
 function App() {
   return (
     <div >
       < Jumbotron />
       < SearchContainer />
+      < SearchResults />
 
     </div>
   );
---
+++ src/components/SearchResults/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function searchResults () {
+    return (
+        <div className={"col-lg-3 searchResults"}>
+            <h2>Hello</h2>
+
+        </div>
+    )
+};
+
+export default searchResults;
