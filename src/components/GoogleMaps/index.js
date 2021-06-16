660e5780b90fe266a0217dc568a3f2db3a18d2e3
Anthony Guerrero <adguerrero1935@gmail.com>
2021-06-16 16:16:15 -0400

    Working on when Marker is dragged to a new location, it well pick up the new address

--- src/components/GoogleMaps/index.js
+++ src/components/GoogleMaps/index.js
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import "./style.css";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 import GeoCode from "react-geocode";
+// import { stat } from "fs";
 
 
 GeoCode.setApiKey("AIzaSyDI7YpCgc7cKTL4XZEo0jN5PzXDCXlHPGs")
+
+
+// GeoCode.setApiKey ="AIzaSyDI7YpCgc7cKTL4XZEo0jN5PzXDCXlHPGs";
 
 function GoogleLocator() {
 
@@ -26,32 +30,98 @@ function GoogleLocator() {
         }
     });
 
-   var onMarkerDragEnd = (event) => {
+
+    // var getCity = (addressArray) => {
+    //     let city = "";
+    //     for (var i = 0; i < addressArray.length; i++) {
+    //         if (addressArray[i].types[0] && "administrative_area_level_2" === addressArray[i].types[0]) {
+    //             city = addressArray[i].long_name;
+    //             return city;
+    //         }
+    //     }
+    // };
+
+
+    // var getArea = (addressArray) => {
+    //     let area = "";
+    //     for (var i = 0; i < addressArray.length; i++) {
+    //         if (addressArray[i].types[0]) {
+    //             for (var a = 0; a < addressArray.length; a++) {
+    //                 if ("sublocality_level_1" === addressArray[i].types[a] || "locality" === addressArray[i].types[a]) {
+    //                     area = addressArray[i].long_name;
+    //                     return area
+    //                 }
+    //             }
+    //         }
+    //     }
+    // };
+
+
+    // var getState = (addressArray) => {
+    //     let state = "";
+    //     for (var i = 0; i < addressArray.length; i++) {
+    //         for (var i = 0; i < addressArray.length; i++) {
+    //             if (addressArray[i].types[0] && "adminstrative_area_level_1" === addressArray[i].types[0]) {
+    //                 state = addressArray[i].long_name;
+    //                 return state
+    //             }
+    //         }
+    //     }
+
+    // };
+
+
+    var onMarkerDragEnd = (event) => {
 
         const newLat = event.latLng.lat();
         const newLng = event.latLng.lng();
 
-        GeoCode.fromLatLng(newLat , newLng)
-        .then(response => {
-            console.log(response);
-        })
-       
+        GeoCode.fromLatLng(newLat, newLng)
+            .then(response => {
+
+                // var address = response.results[0].formatted_address
+
+                // const address = response.results[0].formatted_address,
+                //     addressArray = response.results[0].address_compoments,
+                //     city = getCity(addressArray),
+                //     area = getArea(addressArray),
+                //     state = getState(addressArray)
+                //     // NOT WORKING!
+
+                // googleLocation.SetState({
+                //     address: (address) ? address : "",
+                //     area: (area) ? area : "",
+                //     city: (city) ? city : "",
+                //     state: (state) ? state : "",
+                //     markerPosition: {
+                //         lat: newLat,
+                //         lng: newLng
+                //     },
+                //     mapPosition: {
+                //         lat: newLat,
+                //         lng: newLng,
+
+                //     }
+                // })
+                console.log(response.results[0].formatted_address);
+            })
+
     };
 
 
-    const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+    var MapWithAMarker = withScriptjs(withGoogleMap(props =>
         <GoogleMap
-            defaultZoom={8}
-            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            defaultZoom={11}
+            defaultCenter={{ lat: 28.6278, lng: -81.3631 }}
         >
             <Marker
-                position={{ lat: -34.397, lng: 150.644 }}
+                position={{ lat: 28.6278, lng: -81.3631 }}
                 draggable={true}
                 onDragEnd={onMarkerDragEnd}
             >
                 <InfoWindow>
                     <div>
-                        <h3>Info displays here!!!!</h3>
+                        <h3>Info goes here</h3>
                     </div>
                 </InfoWindow>
             </Marker>
@@ -60,6 +130,7 @@ function GoogleLocator() {
 
     return (
         <div className={"col-lg-6 googleMaps"}>
+
             <MapWithAMarker
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDI7YpCgc7cKTL4XZEo0jN5PzXDCXlHPGs&v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: `100%` }} />}
